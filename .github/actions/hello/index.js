8a6f43635fe68204bf23dab9b7c885604dd3ca3a
Juan Camilo Camargo <camilocm_67@hotmail.com>
2022-12-13 21:04:19 -0500

    first own action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,14 @@
+import core from '@actions/core';
+import github from '@actions/github';
+try {
+//throw(new Error("Some error"));
+const whoToGreet = core.getInput('who-to-greet');
+console.log(`Hello ${whoToGreet}`);
+
+const time = new Date();
+core.setOutput("time",time.toTimeString());
+console.log(JSON.stringify(github,null,'\t'));
+} catch (error) {
+    core.setFailed(error.message);
+}
+
