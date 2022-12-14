4af2e4d77659a5bb5d9954f6cba71a2d6aa2d086
Juan Camilo Camargo <camilocm_67@hotmail.com>
2022-12-13 21:16:10 -0500

    fix3

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1793,7 +1793,7 @@ const time = new Date();
 _actions_core__WEBPACK_IMPORTED_MODULE_0___default().setOutput("time",time.toTimeString());
 console.log(JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_1___default.a,null,'\t'));
 } catch (error) {
-_actions_github__WEBPACK_IMPORTED_MODULE_1___default().setFailed(error.message);
+_actions_core__WEBPACK_IMPORTED_MODULE_0___default().setFailed(error.messaage);
 }
 
 
